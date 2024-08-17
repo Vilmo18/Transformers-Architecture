525045c32ed80ad625a16274bb36ac0b4f70f44c
Vilmo18 <kvilmorin@gmail.com>
2024-08-17 23:35:24 +0200

    upadate

--- config/train_shakespeare.py
+++ config/train_shakespeare.py
@@ -16,13 +16,12 @@ wandb_run_name = 'mini-gpt'
 dataset = 'shakespeare'
 gradient_accumulation_steps = 1
 batch_size = 64
-block_size = 256 # context of up to 256 previous characters
+block_size = 512 # context of up to 256 previous characters
 
 # baby GPT model :)
 n_layer = 6
 n_head = 6
 n_embd = 384
-vocab_size = 32000
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
