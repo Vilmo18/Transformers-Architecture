188fcc5ff2e77cd4f667ff3b1005cd5fa8571d30
Vilmo18 <kvilmorin@gmail.com>
2024-08-17 17:54:12 +0200

    add readme

--- config/train_shakespeare.py
+++ config/train_shakespeare.py
@@ -22,6 +22,7 @@ block_size = 256 # context of up to 256 previous characters
 n_layer = 6
 n_head = 6
 n_embd = 384
+vocab_size = 30000
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
