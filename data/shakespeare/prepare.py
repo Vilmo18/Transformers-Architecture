1b88acd03d721d06a1eb09f545163b9e9c125d3c
Vilmo18 <kvilmorin@gmail.com>
2024-08-18 17:41:19 +0200

    update

--- data/shakespeare/prepare.py
+++ data/shakespeare/prepare.py
@@ -39,22 +39,59 @@ val_data = data[int(n*0.9):]
 #val_ids = enc.encode_ordinary(val_data)
 tokenizer = AutoTokenizer.from_pretrained("YvanCarre/anonym-tokenizer")
 #tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-train_ids = tokenizer.encode(train_data, add_special_tokens=False)
-val_ids = tokenizer.encode(val_data, add_special_tokens=False)
 
 
-print(f"train has {len(train_ids):,} tokens")
-print(f"val has {len(val_ids):,} tokens")
+def process(example):
 
-# export to bin files
-train_ids = np.array(train_ids, dtype=np.uint16)
-val_ids = np.array(val_ids, dtype=np.uint16)
-train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
-val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
+    ids = tokenizer.encode(example['article'], add_special_tokens=False)  
+    ids.append(tokenizer.eos_token_id)  
 
-# train.bin has 301,966 tokens
-# val.bin has 36,059 tokens
-# save the meta information as well, to help us encode/decode later
+    out = {'ids': ids, 'len': len(ids)}
+    return out
+
+tokenized = repo.map(
+        process,
+        remove_columns=['article','highlights', 'id'],
+        desc="tokenizing the splits",
+        num_proc=8,
+    )
+
+for split, dset in tokenized.items():
+        arr_len = np.sum(dset['len'], dtype=np.uint64)
+        filename = os.path.join(os.path.dirname(__file__), f'{split}.bin')
+        dtype = np.uint16 # (can do since enc.max_token_value == 50256 is < 2**16)
+        arr = np.memmap(filename, dtype=dtype, mode='w+', shape=(arr_len,))
+        total_batches = 1024
+
+        idx = 0
+        for batch_idx in tqdm(range(total_batches), desc=f'writing {filename}'):
+            # Batch together samples for faster write
+            batch = dset.shard(num_shards=total_batches, index=batch_idx, contiguous=True).with_format('numpy')
+            arr_batch = np.concatenate(batch['ids'])
+            # Write into mmap
+            arr[idx : idx + len(arr_batch)] = arr_batch
+            idx += len(arr_batch)
+        arr.flush()
+
+
+
+
+
+
+# train_ids = tokenizer.encode(train_data, add_special_tokens=False)
+# val_ids = tokenizer.encode(val_data, add_special_tokens=False)
+
+
+# print(f"train has {len(train_ids):,} tokens")
+# print(f"val has {len(val_ids):,} tokens")
+
+# # export to bin files
+# train_ids = np.array(train_ids, dtype=np.uint16)
+# val_ids = np.array(val_ids, dtype=np.uint16)
+# train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
+# val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
+
+
 vocab_size = 32001
 meta = {
     'vocab_size': vocab_size,
