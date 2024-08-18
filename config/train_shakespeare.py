b2d040364059a91a9fcc00f4e48b0fe525575b31
Vilmo18 <kvilmorin@gmail.com>
2024-08-18 02:21:13 +0200

    update

--- config/train_shakespeare.py
+++ config/train_shakespeare.py
@@ -39,10 +39,10 @@ lr_decay_iters = 600000
 weight_decay = 1e-1
 
 
-learning_rate = 1e-3 # with baby networks can afford to go a bit higher
+learning_rate = 1e-4 # with baby networks can afford to go a bit higher
 max_iters = 5000
 lr_decay_iters = 5000 # make equal to max_iters usually
-min_lr = 1e-4 # learning_rate / 10 usually
+#min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
 # warmup_iters = 100 # not super necessary potentially
